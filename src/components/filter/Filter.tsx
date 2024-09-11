9c8a0f6d4cec9b1afb0ad9d98e2631cb4a46b2dc
giorgigio1 <giorgigiorgadze93@gmail.com>
2024-09-11 19:07:51 +0400

    added header

---
+++ src/components/filter/Filter.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import bina from "../../images/bina.png";
+
+const Filter = () => {
+  return (
+    <div>
+        <select name="" id="">
+            <option value="">aa</option>
+            <option value="">abb</option>
+            <option value="">abcc</option>
+        </select>
+    </div>
+  );
+};
+
+export default Filter;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import logo from "../../images/logo.png";
+import "../../styles/header.css";
+
+const Header = () => {
+  return (
+    <figure className="figure">
+      <img className="img" src={logo} alt="" />
+    </figure>
+  );
+};
+
+export default Header;
